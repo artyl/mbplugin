4c20a195c3b7114f0afb7066ae9bf4b8f1628639
Artem Lavrenov <ArtemLavrenov@gmail.com>
2020-06-27 09:27:15 +0300

    fix strelka emergencyblocked error

--- plugin/strelka.py
+++ plugin/strelka.py
@@ -9,15 +9,15 @@ def get_balance(login, password, storename=None):
     result = {}
     session = requests.Session()
     baseurl = 'http://strelkacard.ru'
-    url = "https://strelkacard.ru/api/cards/status/?cardnum=" + \
-        login+"&cardtypeid=3ae427a1-0f17-4524-acb1-a3f50090a8f3"
-    response2 = session.get(
-        url, headers={'Referer': baseurl + '/'}, verify=False)
+    url = "https://strelkacard.ru/api/cards/status/?cardnum=" + login + "&cardtypeid=3ae427a1-0f17-4524-acb1-a3f50090a8f3"
+    response2 = session.get(url, headers={'Referer': baseurl + '/'}, verify=False)
     logging.debug(response2.text)
     # html=u'<html><http-equiv="Content-type" content="text/html; charset=windows-1251" /><p id="Balance">%s<p id="TarifPlan">%s</html>' % (
     result['Balance'] = response2.json()['balance']/100.
-    if response2.json()['emergencyblocked']:
-        result['BlockStatus'] = 'Eemergency Blocked'
+    if response2.json().get('emergencyblocked', False):
+        result['BlockStatus'] = 'Emergency Blocked'
+    if response2.json().get('cardblocked', False):
+        result['BlockStatus'] = 'Card Blocked'
     return result
 
 
