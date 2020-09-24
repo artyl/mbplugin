2a3cf253f00dc32564f6d0c0b448fd926fec760d
Artem Lavrenov <git@lavrenov.net>
2020-09-24 19:40:56 +0300

    standalone version

--- standalone/check_balance.bat
+++ standalone/check_balance.bat
@@ -1,3 +1,3 @@
 cd mbplugin\plugin
-..\python\python.exe mbplugin.py p_bububu 9161112233 bbbbb
-..\python\python.exe mbplugin.py p_bububu 9162223344 bbbbb
+..\python\python.exe mbplugin.py p_test1 9161112233 bbbbb
+..\python\python.exe mbplugin.py p_test3 demo@saures.ru demo
