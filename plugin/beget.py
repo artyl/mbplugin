21bc7678171ed8d9dafbb003df818e7a1c8a2e0c
Artem Lavrenov <ArtemLavrenov@gmail.com>
2021-09-29 20:21:32 +0300

    plugin beget for beget.com hosting provider author d1mas

---
+++ plugin/beget.py
@@ -0,0 +1,42 @@
+#проверка баланса хостинг-провайдера BEGET
+#https://beget.com/ru
+#https://beget.com/ru/kb/api/beget-api
+# -*- coding: utf8 -*-
+''' Автор d1mas '''
+import os, sys, re, logging
+import store
+
+def get_balance(login, password, storename=None):
+    logging.info(f'start get_balance {login}')
+    result = {}
+    url = 'https://api.beget.com/api/user/getAccountInfo?login=' + login + '&passwd=' + password + '&output_format=json'
+    session = store.Session(storename)
+    response = session.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(f'Login error: status_code {response.status_code}!=200')
+    # Не отдают они content-type=json в заголовках
+    #if 'json' not in response.headers.get('content-type'):
+    #    raise RuntimeError(f'Login error: {response.text}')
+
+    # Разбираем полученный json
+    if response.json()['status'] != 'success':
+        raise RuntimeError(f'Login error: reply {response.text}')
+    logging.info(f'Parsing data')
+    data = response.json()['answer']['result']
+    result['Balance'] = data['user_balance']
+
+    try:
+        result['TarifPlan'] =  data['plan_name']
+    except Exception:
+        logging.info(f'Not found TarifPlan')
+    try:
+        result['TurnOff'] = data['user_days_to_block']
+    except Exception:
+        logging.info(f'Not found TurnOff')
+
+    session.save_session()
+    return result
+
+
+if __name__ == '__main__':
+    print('This is module beget')
