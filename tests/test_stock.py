66b13902b28429017a7a1a237380a7e51fced542
Artem Lavrenov <ArtemLavrenov@gmail.com>
2022-10-15 20:22:05 +0300

    test_stock

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 import pytest
 import conftest  # type: ignore # ignore import error
 import settings, stock  # pylint: disable=import-error
@@ -7,10 +7,14 @@ import settings, stock  # pylint: disable=import-error
 def setup():
     os.makedirs(os.path.join(settings.mbplugin_root_path, 'mbplugin', 'log'), exist_ok=True)
     os.makedirs(os.path.join(settings.mbplugin_root_path, 'mbplugin', 'store'), exist_ok=True)
+    assert os.path.split(__file__)[0] == os.path.split(settings.mbplugin_ini_path)[0], f'Folder {settings.mbplugin_ini_path} is not subfolder for test {__file__}'
+    shutil.copyfile(os.path.join(settings.mbplugin_ini_path, 'mbplugin.ini.ori'), os.path.join(settings.mbplugin_ini_path, 'mbplugin.ini'))
 
 
 def teardown():
-    pass # print ("basic teardown into conftest")
+    assert os.path.split(__file__)[0] == os.path.split(settings.mbplugin_ini_path)[0], f'Folder {settings.mbplugin_ini_path} is not subfolder for test {__file__}'
+    os.remove(os.path.join(settings.mbplugin_ini_path, 'mbplugin.ini'))
+    # print ("basic teardown into conftest")
 
 
 def test_stock():
