6c53f04024bb53c748c0dd2a81a82d59dc120f64
Artem Lavrenov <ArtemLavrenov@gmail.com>
2023-08-19 18:49:26 +0300

    fix beeline web form login

--- plugin/beeline.py
+++ plugin/beeline.py
@@ -4,17 +4,18 @@ import os, sys, re, time, logging
 import store, settings, browsercontroller
 
 icon = '789C7D93CB6B135114C6BF79C4BC66924993D0269926D3247DD8579A5A92D61A5B84B6282816AC8FADB890BAB3E24E37EE15D48505F11FD045C54AC577B1540AC5A255B1B4A2958AF85AE9C23E92F8CD344A15F4901FDF9D7BE79C09E7BB67C7EE8C0C2B32A48E68250418EB07A5F3FF45454505CACBCBE1F3F9204912445182D7EB4520184490FC2B3C5E0F227A04A220941021992A8A10A842692F180C4051953F72BD9A86B2AA66A8B12628B14628C63AEEAA46A8550D50889AA026EAE149D6A38C6BD5ABFDCE8F1C3D0F5C2F02770BC03875823A459D26B3E42599E7DE5BEA12799F8776E6A295AB376D012E7F07AEFD004696811B2BC02D726F15784026F2C063324D9EB2C67332CFF5B365045ADA103A3E0CDCE4F31D729F678F587FB2F4ED19F2827BAFA8AFC922F9403E93AF79F8CF0DA332D50E776D3BD41A6A5D164EE2DA9C85BB210BA521034F6316BEE60E78531D50F9AE92CEC0D5DA0E676B16C18616C8A20C4114D86BD1EAAF60F94DB8762B2AC2E13092890489731DB27A2F0A1264D31F7A63936DD6B359E39777FCC1ED70221AD5D19A4E23B77D1B72B91CD22D2984F5305C7607BFCB7C4940B47B2F94ADFBA012676E006EE2EC1A40B0E720D20347D03B7812FD43A7D07FE2347A8E0D2175E830427B0EC0BE6B3FFCDD7D080F5D0246D9E7DB66FFA8E3D4C95548336B48CEADA16BB180BEA53C7AD9B7EE8F45843E156037FBF7A580E0D96118CD6DC0956FC0557A364246C9D88A55273E9547E76C013BE75863BE884E7A1059A01F0BE6BD584679BACDBA0389C10BF49FDE8F9977883C5CF7D0F1A48824BD6BE1FB29DE9D38FD93DFF1EC4D1E81D2FD3143E3ACE891045C6521B87CC41F81C35F014F4847D848C048D6C0A8AE41C888C3138EC2AFC7E0D1B48D23004DF3A2BAB6D69A1769C30CC9928C4DECB7CD66A75F326246CC9AB57F85611888C562F42ECA1C1B6459E27F8B40AFD42DFE8E9F8F93C73F'
-login_url = 'https://my.beeline.ru'
+login_url = 'https://beeline.ru/login'
 # если залогинены, то попадем сразу в ЛК, иначе попадем ХЗ куда
 direct_lk_url = 'https://beeline.ru/customers/products/elk/tab/mobile-connection'
 profile_url = 'https://beeline.ru/customers/products/mobile/profile/'
 user_selectors = {'chk_lk_page_js': "document.querySelector('div.initial-form')==null && document.querySelector('form input[type=password]')== null",
                   'chk_login_page_js': "!(document.querySelector('div.initial-form')==null && document.querySelector('form input[type=password]')== null)",
-                  'login_clear_js': "document.querySelector('form input[type=text]').value=''",
-                  'login_selector': 'form input[type=text]',
-                  'password_clear_js': "document.querySelector('form input[type=password][role=textbox]').value=''",
-                  'password_selector': 'form input[type=password][role=textbox]',
-                  'submit_js': "document.querySelector('form [type=button]').click()",
+                  'before_login_js': "document.querySelectorAll('button').forEach(el=>el.innerText=='С постоянным паролем'?el.click():0);",
+                  'login_clear_js': "document.querySelector('div.initial-form input[name=login]').value=''",
+                  'login_selector': 'div.initial-form input[name=login]',
+                  'password_clear_js': "document.querySelector('div.initial-form input[name=password]').value=''",
+                  'password_selector': 'div.initial-form input[name=password]',
+                  'submit_js': "document.querySelector('button[type=submit]').click()",
                   }
 profile_tag = 'api/profile/userinfo/data/?noTimeout'
 accumulators2_tag = 'api/uni-profile-mobile/blocks'
