fde79e6b5d607b03b2b20f23e78275373a8149e1
Artyl <artyl@users.noreply.github.com>
2026-01-18 10:16:08 +0300

    mts fix for renamed services

--- plugin/mts.py
+++ plugin/mts.py
@@ -10,7 +10,7 @@ if sys.platform == 'win32':
 icon = '789C75524D4F5341143D84B6A8C0EB2BAD856A4B0BE5E301A508A9F8158DC18498A889896E8C3B638C31F147B83171E34E4388AE5C68E246A3C68D0B5DA82180B5B40A5A94B6F651DA423F012D2DE09D79CF4A207DC949A733F79C39F7CC1D3A37A801FF060912415451058772A09E6FFD04CD18F4DA09C267C214210051FB857EFFC1AFEEB3F3495E2F68DEA35EF396F086F6BCBC46D47E257C2304A1D7045157350DA13A80FA6A1F6AAB7CB4F6AB5A5E08DA71D2F840FC772AEF3B44DD0F1874215A87D1DA34871B57658CDE4F1212B87E2504BBD94F5A01D5938F7B16341F8937CB79C65DBF60DA2DC3E594F1FAE532D64B1BD8DCDCE428D1FAC5B30CDAAD33E483799C2E6B187411E245D124CC63BF18C3DD3BB9326F3B6EDF4A506FB3C49FE5BE99C6DE3D32F6E9636836C671A0631153DEB58AFCC9F155EA4DE951D40579CE8C6B37C5693F895347D388C9EB15F9D148119E1E190D3551F23DC7F366F73A2D4974DA52183E9E831CADCC0F878A38E88AC15C3B4F1A119E5D8B39814EEB125CAD199CF0E4C97FA9227F7CAC809E96382CE4D9489989BA9F7092EF2E7B8A7ACF62D0B58C278F8A15F90F4656D0D29880D5B0C07363EFD6665944B72385012947FC15DCBC56403EB7939BCD6CE0F2852CF193B0352C500F8C1F267EB2CC3FEC5EA10CFFE0D5F39D193C7D5C80BB2DCDEFDBCADFEEFF58FF2A2E9D2FC0F7E9BFC6C45809A74FE62035A778BDE23FCAFD3B28BF0EEB22E597E61E0EF52EE348DF2A2E9EFD8D87236B18BD57C099A13CE596E639B37AF6E66C5E597ECC0B7B7BA97909BDCE0CFA3BB3F074E73906A43CFADA73FC6DBAD4BB597D63DD3C0C35CA0C59049A3D933203926D89DFE3261D779B0217FD67DA2C273667AC9ECDBB323F33F80B823D9864'
 
 # login_url 'https://login.mts.ru/amserver/UI/Login'  # - другая форма логина - там оба поля на одной странице, и можно запомнить сессию
-login_url = 'https://lk.mts.ru'  # а на этой запомнить сессию нельзя, но другой больше нет
+login_url = 'https://lk.mts.ru/tarif'
 
 def get_free_port() -> int:
     """Get free port."""
@@ -427,6 +427,8 @@ def get_balance(login, password, storename=None, wait=True, **kwargs):
     pd = PureBrowserDebug(user_data_dir, response_store_path=response_store_path, log_responses=log_responses, headless_chrome=headless_chrome, show_chrome=show_chrome, chrome_args=chrome_args, wait_screenshot=wait_screenshot, slowdown=slowdown, chrome_executable_path=chrome_executable_path)
     # 1 Is login ???
     store.feedback.text(f"Run browser", append=True)
+    pd.send('Page.navigate', {'url': 'https://id.mts.ru'})
+    time.sleep(4 * pd.slowdown)
     pd.send('Page.navigate', {'url': login_url})
     state = wait_state()
     if state == 'unknown':
@@ -674,7 +676,7 @@ def get_balance(login, password, storename=None, wait=True, **kwargs):
         no_count_services = ['Забугорище', 'БИТ за границей', 'За границей', 'Мессенджеры в путешествии', 'Всё для поездки', 'Пакеты интернета в особых странах']  # услуги которые не считаем в платные, т.к. сбивают с толку
         services_ = [(pr.get('name', ''),pr.get('description', ''),(pr.get('currentPrice') or {}).get('value', 0), (pr.get('currentPrice') or {}).get('unitOfMeasure', 'r'))  for pr in products]
         services_ = services_ + [('Ежемесячная плата ' + tariff_name, tariff_price_date, *tariff_price_text.split(' ',1))]
-        services_2 = [(s, float(str(c).replace(',', '.')) * (30 if '/день' in p else 1)*(0 if s in no_count_services else 1)) for s, d, c, p in services_]
+        services_2 = [(s, float(str(c).replace(',', '.')) * (30 if '/день' in p else 1)*(0 if any([cs for cs in no_count_services if s.startswith(cs)]) else 1)) for s, d, c, p in services_]
         # result['BlockStatus'] = active.get('data', {}).get('accountBlockStatus', '').replace('Unblocked', '')
         try:
             services = sorted(services_2, key=lambda i: (-i[1], i[0]))
